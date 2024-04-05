3a3671cab45f5e33b9145d6f99c4743018dbe45e
Jaemin.Park <once29@naver.com>
2024-04-05 17:26:03 +0900

    Remove unnecessary dependencies from external module

--- raisedragon-external/build.gradle.kts
+++ raisedragon-external/build.gradle.kts
@@ -1,9 +1,4 @@
 dependencies {
-    implementation(project(":raisedragon-core"))
-
-    implementation("org.springframework.boot:spring-boot-starter-validation:3.0.4")
-    implementation("org.springframework.boot:spring-boot-starter-aop:3.0.4")
-
     // aws
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 }
