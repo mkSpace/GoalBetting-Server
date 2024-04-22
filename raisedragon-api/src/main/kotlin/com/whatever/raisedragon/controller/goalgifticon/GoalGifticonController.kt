5ccb645eb9b6a85903520c6bba24f8545b272149
Jaemin.Park <once29@naver.com>
2024-04-22 18:46:14 +0900

    Fix wrong types of response status in GoalGifticon api

--- raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goalgifticon/GoalGifticonController.kt
+++ raisedragon-api/src/main/kotlin/com/whatever/raisedragon/controller/goalgifticon/GoalGifticonController.kt
@@ -36,7 +36,6 @@ class GoalGifticonController(
         )
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @Operation(
         summary = "다짐 내 기프티콘 조회 API",
         description = "다짐 내 기프티콘 조회 (나의 기프티콘 이거나 승리한 경우 조회 가능)",
@@ -52,7 +51,6 @@ class GoalGifticonController(
         )
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @Operation(
         summary = "다짐 내 기프티콘 수정 API",
         description = "다짐 내 기프티콘 수정",
